4355c805e1f3fc87f6cf41db74e0105ede3d886d
Ivan <125994256+pickless141@users.noreply.github.com>
2024-01-16 15:16:17 -0300

    add: configuracion de nodemailer y controler para enviar emails a pacientes

--- controllers/citas/citasController.js
+++ controllers/citas/citasController.js
@@ -1,6 +1,7 @@
 const Cita = require('../../models/citas/Citas');
 const verificarAutenticacion = require('../../middlewares/authMiddleware');
 const Paciente = require('../../models/pacientes/Paciente');
+const transporter = require('../../config/mailerConfig');
 
 const registrarCita = async (req, res) => {
   try {
@@ -22,12 +23,23 @@ const registrarCita = async (req, res) => {
 
       await nuevaCita.save();
 
+      // Enviar correo de confirmación al paciente
+      const mensajeCorreo = {
+        from: process.env.GMAIL_USER,
+        to: paciente.email,
+        subject: 'Confirmación de Cita',
+        text: `Su cita para el ${fecha} a las ${hora} ha sido registrada exitosamente.`,
+      };
+
+      await transporter.sendMail(mensajeCorreo);
+
+      // Obtener la cita registrada con la información del paciente
       const citaRegistrada = await Cita.findById(nuevaCita._id).populate('paciente');
 
       res.status(201).json({ mensaje: 'Cita registrada exitosamente', cita: citaRegistrada });
     });
   } catch (error) {
-    console.error('Error al registrar la cita:', error);
+    console.error('Error al registrar la cita:', error.message);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 };
