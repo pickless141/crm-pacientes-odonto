bf9084e796d378fb184c00b36a59869ad36a6ad8
Ivan <125994256+pickless141@users.noreply.github.com>
2024-01-19 18:11:34 -0300

    add: ruta para traer todas las citas registradas

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //const crypto = require('crypto');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mainRoutes = require('./routes/indexRoutes')
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 app.use('/', mainRoutes)
